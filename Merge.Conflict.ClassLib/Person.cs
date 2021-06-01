7161084eab5d66c6606f89928964ccdf66dc4028
Nirav Bhatt <nirav.bhatt@allscripts.com>
2021-06-01 19:50:46 +0530

    First draft

---
+++ Merge.Conflict.ClassLib/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Merge.Conflict.ClassLib
+{
+    /// <summary>
+    /// This is my Person class
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Name of the person.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
